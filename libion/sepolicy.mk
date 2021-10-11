6f0bb38329b630eae0a63f135b0a5a61f999161a
Dmitrii <bankersenator@gmail.com>
2021-10-11 09:15:51 +0000

    sepolicy: Adapt paths for VoltageOS
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>

--- libion/sepolicy.mk
+++ libion/sepolicy.mk
@@ -1,7 +1,7 @@
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/libion/vendor
+    device/voltage/sepolicy/libion/vendor
 endif
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/libion/system_ext/private
+   device/voltage/sepolicy/libion/system_ext/private
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -10,16 +10,16 @@ endif
 endif
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/private
+    device/voltage/sepolicy/qcom/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/dynamic \
-    device/lineage/sepolicy/qcom/system
+    device/voltage/sepolicy/qcom/dynamic \
+    device/voltage/sepolicy/qcom/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/dynamic \
-    device/lineage/sepolicy/qcom/vendor
+    device/voltage/sepolicy/qcom/dynamic \
+    device/voltage/sepolicy/qcom/vendor
 endif
 
 ifeq (,$(filter device/qcom/sepolicy-legacy-um/legacy/vendor/common device/qcom/sepolicy_vndr/legacy-um/legacy/vendor/common, $(BOARD_VENDOR_SEPOLICY_DIRS)))
